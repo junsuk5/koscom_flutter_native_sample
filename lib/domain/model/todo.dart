30d256b53a30c879322021e8f038aa43c4d26da2
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-11-21 13:28:32 +0900

    모델 클래스 작성

---
+++ lib/domain/model/todo.dart
@@ -0,0 +1,37 @@
+class Todo {
+  final int userId;
+  final int id;
+  final String title;
+  final bool completed;
+
+  const Todo({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.completed,
+  });
+
+  @override
+  String toString() {
+    return 'Todo(userId: $userId, id: $id, title: $title, completed: $completed)';
+  }
+
+  // Optional: JSON serialization methods
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      completed: json['completed'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'userId': userId,
+      'id': id,
+      'title': title,
+      'completed': completed,
+    };
+  }
+}
