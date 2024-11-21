1ec381368add7f060d637628dd0e5db00c9713d9
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-11-21 14:57:29 +0900

    네이티브 쪽 MethodChannel 작성 완료

--- android/app/src/main/kotlin/com/survivalcoding/flutter_native_sample/MainActivity.kt
+++ android/app/src/main/kotlin/com/survivalcoding/flutter_native_sample/MainActivity.kt
@@ -1,5 +1,33 @@
 package com.survivalcoding.flutter_native_sample
 
+import androidx.lifecycle.lifecycleScope
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import kotlinx.coroutines.launch
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val channel = "survivalcoding.com/todo"
+
+    private val todoRepository = TodoRepository()
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            channel,
+        ).setMethodCallHandler { call, result ->
+            if (call.method == "getTodo") {
+                lifecycleScope.launch {
+                    val response = todoRepository.getTodo(
+                        call.argument<Int>("id") ?: throw IllegalArgumentException()
+                    )
+                    result.success(response.body())
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
